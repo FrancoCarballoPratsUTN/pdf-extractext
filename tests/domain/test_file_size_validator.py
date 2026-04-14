5bcf16f1f14bafb315da28f9c30ced74009f1414
Macarena Michaux <45722931@alumnos.frsr.utn.edu.ar>
2026-04-14 17:16:56 -0300

    refactor(tests): Refactorice tets_file_validator #6

--- tests/domain/test_file_size_validator.py
+++ tests/domain/test_file_size_validator.py
@@ -7,9 +7,9 @@ def test_validate_file_size_success():
     validate_file_size(file_size)
 
 def test_validate_file_size_fails_when_empty():
-        file_size = 0
-        validate_file_size(file_size)
+    file_size = 0
+    validate_file_size(file_size)
 
 def test_validate_file_size_fails_when_exceeds_limit():
-        file_size = MAX_FILE_SIZE_BYTES + 1
-        validate_file_size(file_size)
+    file_size = MAX_FILE_SIZE_BYTES + 1
+    validate_file_size(file_size)
