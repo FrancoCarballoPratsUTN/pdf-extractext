9a23fdb3622d6c912914a7b276a6e2d03c05ca1a
Macarena Michaux <45722931@alumnos.frsr.utn.edu.ar>
2026-04-18 18:33:03 -0300

    feat(domain): agregue la validacion que detecta si el pdf solo tiene imagen #7

---
+++ app/domain/use_cases/verifications/file_only_has_imagen.py
@@ -0,0 +1,20 @@
+from pypdf import PdfReader
+import io
+
+import io
+from pypdf import PdfReader
+
+def file_has_imagen(pdf_content: bytes) -> bool:
+    """
+    Checks if the PDF is an image
+    Returns True if NO extractable text is found.
+    Returns False if the file contains text.
+    """
+    reader = PdfReader(io.BytesIO(pdf_content))
+    accumulated_text = ""
+    
+    for page in reader.pages:
+        text = page.extract_text()
+        if text:
+            accumulated_text += text       
+    return len(accumulated_text.strip()) == 0
