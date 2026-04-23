3d91a2dfa31be6bde9a661c62bc8905c2a74b372
Macarena Michaux <45722931@alumnos.frsr.utn.edu.ar>
2026-04-23 20:41:38 -0300

    feat(domain): Use case find #16

---
+++ app/domain/use_cases/crud/find_use_case.py
@@ -0,0 +1,9 @@
+from app.infrastructure.persistence.document_repository import DocumentRepository
+
+class FindDocumentUseCase:
+
+    def __init__(self, repository: DocumentRepository):
+        pass
+
+    def execute(self, document_id):
+        pass
